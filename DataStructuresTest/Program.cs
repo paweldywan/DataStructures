9c27767991eefa3ddef6bfdb96595a3220744bab
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-06-25 23:29:51 +0200

    Add project files.

---
+++ DataStructuresTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = new CircularBuffer(capacity: 3);
+
+            while(true)
+            {
+                var parsed = false;
+                var value = 0.0;
+                var input = Console.ReadLine();
+
+                if(double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+
+                break;
+            }
+
+            Console.WriteLine("Buffer: ");
+
+            while(!buffer.IsEmpty)
+            {
+                Console.WriteLine("\t" + buffer.Read());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
